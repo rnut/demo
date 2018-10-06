c2ec4153e4ba888b429cfecefc7fa257e1c7c182
rnut_k <rnut.ist@gmail.com>
2018-10-06 09:11:59 +0700

    change database url

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ var admin = require('firebase-admin');
 var serviceAccount = require('/env/dv-demo-villa-firebase-adminsdk-dc3nk-b4e6b37c96.json');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
+    databaseURL: "https://dv-demo-villa.firebaseio.com"
 });
 // ==================================================================================
