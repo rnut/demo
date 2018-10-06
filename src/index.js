e5712e83c295cc819b61e7249097319badef8c09
rnut_k <rnut.ist@gmail.com>
2018-10-06 09:55:41 +0700

    retrieve data from firestore

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@
 // ===============================> SETUP FIREBASE ==================================
 var admin = require('firebase-admin');
 var serviceAccount = require('./env/dv-demo-villa-firebase-adminsdk-dc3nk-b4e6b37c96.json');
-admin.initializeApp({
+var fir = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://dv-demo-villa.firebaseio.com"
 });
 // ==================================================================================
-
-console.log(['app_name', admin.app().name]);
+console.log('app_name->', admin.app().name);
+var db = admin.firestore();
+db.settings({timestampsInSnapshots: true})
+db.collection("Users").get('users-01').then((snapshot) => {
+    snapshot.forEach((doc) => {
+        console.log('----------------------------->')
+        console.log(doc.id, '=>', doc.data());
+    });
+  })
+  .catch((err) => {
+    console.log('Error getting documents', err);
+  });
