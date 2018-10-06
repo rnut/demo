8af10348ee3f4948a00e88d6f7d46e77fa8e2505
rnut_k <rnut.ist@gmail.com>
2018-10-06 09:42:09 +0700

    change path env for set credential

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 
 // ===============================> SETUP FIREBASE ==================================
 var admin = require('firebase-admin');
-var serviceAccount = require('/env/dv-demo-villa-firebase-adminsdk-dc3nk-b4e6b37c96.json');
+var serviceAccount = require('./env/dv-demo-villa-firebase-adminsdk-dc3nk-b4e6b37c96.json');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://dv-demo-villa.firebaseio.com"
 });
 // ==================================================================================
 
-console.log(admin.app().name);
+console.log(['app_name', admin.app().name]);
